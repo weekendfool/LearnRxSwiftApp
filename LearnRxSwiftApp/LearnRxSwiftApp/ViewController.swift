cb844ef94d74346b642a1c33211b467be06ff273
尾原徳泰 <nori.tukareta@gmail.com>
2022-05-19 23:00:12 +0900

    RxSwiftインストール RxCocoaインストール

---
+++ LearnRxSwiftApp/LearnRxSwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnRxSwiftApp
+//
+//  Created by Oh!ara on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LearnRxSwiftApp/Pods/Target Support Files/Pods-LearnRxSwiftAppTests/Pods-LearnRxSwiftAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LearnRxSwiftAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_LearnRxSwiftAppTests
+@end
