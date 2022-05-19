0b2b22083caf2c2f58f5ed1789b4f8f220f7bba3
尾原徳泰 <nori.tukareta@gmail.com>
2022-05-20 00:45:08 +0900

    createSample実装

--- LearnRxSwiftApp/LearnRxSwiftApp/ViewController.swift
+++ LearnRxSwiftApp/LearnRxSwiftApp/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createSample()
     }
 
+    // MARK: - 関数
+    func createSample() {
+        // 購読の破棄のコントロール
+        let disposebag = DisposeBag()
+        
+        // マニュアルの作成
+        let observable = Observable<String>.create { observer in
+            observer.onNext("か")
+            observer.onNext("き")
+            observer.onNext("く")
+            observer.onCompleted()
+            
+            return Disposables.create {
+                print("Observer dispose")
+            }
+        }
+        
+        // observerの購読
+        observable.subscribe { element in
+            print("Observer: \(element)")
+        } onDisposed: {
+            print("Observer Disposed")
+        }.disposed(by: disposebag)
+
+    }
+    
 
 }
 
